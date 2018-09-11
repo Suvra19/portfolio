c819dc041700a071304a46e34a14776e685964b3
Subhra <subhra.306@gmail.com>
2018-09-11 13:43:33 +1200

    added thank you message for form submission.

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ const navIcon = document.querySelector(".navigation__checkbox")
 navElements.addEventListener("click", (e) => {
     navIcon.checked = false;
 })
+
+window.onload = () => {
+    let urlArr = window.location.search.split('?')
+    console.log(urlArr)
+    if (urlArr.length === 2 && urlArr[1] === 'showMessage') {
+        let message = document.querySelector(".message")
+        message.classList.remove('message__hidden');
+        setTimeout(() => {
+            message.classList.add('message__hidden');
+        }, 3000)
+    }
+}
